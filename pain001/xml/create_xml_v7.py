402373918228d2fc990b85398cbf0eec85b3adfc
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2024-05-19 18:46:56 +0100

    fix(pain001): :bug: fix unused import os + add copyrights

--- pain001/xml/create_xml_v7.py
+++ pain001/xml/create_xml_v7.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Sebastien Rousseau.
+# Copyright (C) 2023-2024 Sebastien Rousseau.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
